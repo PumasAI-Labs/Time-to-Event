ace3b6a5f0c33daca55cc651f2ce4d6122592562
jotas6 <j.oneto@pumas.ai>
2023-08-31 10:56:16 -0500

    Apply JuliaFormatter

--- 01-single_tte.jl
+++ 01-single_tte.jl
@@ -12,146 +12,142 @@ tte_single = dataset("tte_single")
 
 # Pumas Modeling
 pop_single = read_pumas(
-    tte_single;
-    observations=[:DV],
-    covariates=[:DOSE],
-    id=:ID,
-    time=:TIME,
-    amt=:AMT,
-    evid=:EVID
+  tte_single;
+  observations = [:DV],
+  covariates = [:DOSE],
+  id = :ID,
+  time = :TIME,
+  amt = :AMT,
+  evid = :EVID,
 )
 
 # Exponential
 tte_single_exp_model = @model begin
-    @param begin
-        λ₁ ∈ RealDomain(; lower=0, init=0.001) # basal hazard
-        β ∈ RealDomain(; init=0.001)           # fixed effect DOSE
-        ω ∈ RealDomain(; lower=0)              # inter-subject variability
-    end
+  @param begin
+    λ₁ ∈ RealDomain(; lower = 0, init = 0.001) # basal hazard
+    β ∈ RealDomain(; init = 0.001)           # fixed effect DOSE
+    ω ∈ RealDomain(; lower = 0)              # inter-subject variability
+  end
 
-    @random begin
-        η ~ Normal(0.0, ω)
-    end
+  @random begin
+    η ~ Normal(0.0, ω)
+  end
 
-    @covariates DOSE
+  @covariates DOSE
 
-    @pre begin
-        _λ₁ = λ₁ * exp(η)          # basal hazard with inter-subject variability
-        _λ₀ = _λ₁ * exp(β * DOSE)  # total hazard
-    end
+  @pre begin
+    _λ₁ = λ₁ * exp(η)          # basal hazard with inter-subject variability
+    _λ₀ = _λ₁ * exp(β * DOSE)  # total hazard
+  end
 
-    @vars begin
-        # exponential
-        λ = _λ₀
-    end
+  @vars begin
+    # exponential
+    λ = _λ₀
+  end
 
-    @dynamics begin
-        # the derivative of cumulative hazard is equal to hazard
-        Λ' = λ
-    end
+  @dynamics begin
+    # the derivative of cumulative hazard is equal to hazard
+    Λ' = λ
+  end
 
-    @derived begin
-        DV ~ @. TimeToEvent(λ, Λ) # special type of distribution
-    end
+  @derived begin
+    DV ~ @. TimeToEvent(λ, Λ) # special type of distribution
+  end
 end
 
-tte_single_exp_fit = fit(
-    tte_single_exp_model,
-    pop_single,
-    init_params(tte_single_exp_model),
-    LaplaceI(),
-)
+tte_single_exp_fit =
+  fit(tte_single_exp_model, pop_single, init_params(tte_single_exp_model), LaplaceI())
 
 # Weibull
 tte_single_weibull_model = @model begin
-    @param begin
-        λ₁ ∈ RealDomain(; lower=0, init=0.001) # basal hazard
-        β ∈ RealDomain(; init=0.001)           # fixed effect DOSE
-        ω ∈ RealDomain(; lower=0)              # inter-subject variability
-        Κ ∈ RealDomain(; lower=0, init=0.001)  # Weibull shape
-    end
+  @param begin
+    λ₁ ∈ RealDomain(; lower = 0, init = 0.001) # basal hazard
+    β ∈ RealDomain(; init = 0.001)           # fixed effect DOSE
+    ω ∈ RealDomain(; lower = 0)              # inter-subject variability
+    Κ ∈ RealDomain(; lower = 0, init = 0.001)  # Weibull shape
+  end
 
-    @random begin
-        η ~ Normal(0.0, ω)
-    end
+  @random begin
+    η ~ Normal(0.0, ω)
+  end
 
-    @covariates DOSE
+  @covariates DOSE
 
-    @pre begin
-        _Κ = Κ                     # shape parameter
-        _λ₁ = λ₁ * exp(η)          # basal hazard with inter-subject variability
-        _λ₀ = _λ₁ * exp(β * DOSE)  # total hazard
-    end
+  @pre begin
+    _Κ = Κ                     # shape parameter
+    _λ₁ = λ₁ * exp(η)          # basal hazard with inter-subject variability
+    _λ₀ = _λ₁ * exp(β * DOSE)  # total hazard
+  end
 
-    @vars begin
-        # Weibull
-        # 1e-10 for model numerical stability
-        λ = _λ₀ * _Κ * (_λ₀ * t + 1e-10)^(_Κ - 1)
-    end
+  @vars begin
+    # Weibull
+    # 1e-10 for model numerical stability
+    λ = _λ₀ * _Κ * (_λ₀ * t + 1e-10)^(_Κ - 1)
+  end
 
-    @dynamics begin
-        # the derivative of cumulative hazard is equal to hazard
-        Λ' = λ
-    end
+  @dynamics begin
+    # the derivative of cumulative hazard is equal to hazard
+    Λ' = λ
+  end
 
-    @derived begin
-        DV ~ @. TimeToEvent(λ, Λ) # special type of distribution
-    end
+  @derived begin
+    DV ~ @. TimeToEvent(λ, Λ) # special type of distribution
+  end
 end
 
 tte_single_weibull_fit = fit(
-    tte_single_weibull_model,
-    pop_single,
-    init_params(tte_single_weibull_model),
-    LaplaceI(),
+  tte_single_weibull_model,
+  pop_single,
+  init_params(tte_single_weibull_model),
+  LaplaceI(),
 )
 
 # Gompertz
 tte_single_gompertz_model = @model begin
-    @param begin
-        λ₁ ∈ RealDomain(; lower=0, init=0.001) # basal hazard
-        β ∈ RealDomain(; init=0.001)           # fixed effect DOSE
-        ω ∈ RealDomain(; lower=0)              # inter-subject variability
-        Κ ∈ RealDomain(; lower=0, init=0.001)  # Gompertz shape
-    end
+  @param begin
+    λ₁ ∈ RealDomain(; lower = 0, init = 0.001) # basal hazard
+    β ∈ RealDomain(; init = 0.001)           # fixed effect DOSE
+    ω ∈ RealDomain(; lower = 0)              # inter-subject variability
+    Κ ∈ RealDomain(; lower = 0, init = 0.001)  # Gompertz shape
+  end
 
-    @random begin
-        η ~ Normal(0.0, ω)
-    end
+  @random begin
+    η ~ Normal(0.0, ω)
+  end
 
-    @covariates DOSE
+  @covariates DOSE
 
-    @pre begin
-        _Κ = Κ                     # shape parameter
-        _λ₁ = λ₁ * exp(η)          # basal hazard with inter-subject variability
-        _λ₀ = _λ₁ * exp(β * DOSE)  # total hazard
-    end
+  @pre begin
+    _Κ = Κ                     # shape parameter
+    _λ₁ = λ₁ * exp(η)          # basal hazard with inter-subject variability
+    _λ₀ = _λ₁ * exp(β * DOSE)  # total hazard
+  end
 
-    @vars begin
-        # Gompertz
-        λ = _λ₀ * exp(_Κ * t)
-    end
+  @vars begin
+    # Gompertz
+    λ = _λ₀ * exp(_Κ * t)
+  end
 
-    @dynamics begin
-        # the derivative of cumulative hazard is equal to hazard
-        Λ' = λ
-    end
+  @dynamics begin
+    # the derivative of cumulative hazard is equal to hazard
+    Λ' = λ
+  end
 
-    @derived begin
-        DV ~ @. TimeToEvent(λ, Λ) # special type of distribution
-    end
+  @derived begin
+    DV ~ @. TimeToEvent(λ, Λ) # special type of distribution
+  end
 end
 
 tte_single_gompertz_fit = fit(
-    tte_single_gompertz_model,
-    pop_single,
-    init_params(tte_single_gompertz_model),
-    LaplaceI(),
+  tte_single_gompertz_model,
+  pop_single,
+  init_params(tte_single_gompertz_model),
+  LaplaceI(),
 )
 
 # compare MLE estimates
 compare_estimates(;
-    Exponential=tte_single_exp_fit,
-    Weibull=tte_single_weibull_fit,
-    Gompertz=tte_single_gompertz_fit
+  Exponential = tte_single_exp_fit,
+  Weibull = tte_single_weibull_fit,
+  Gompertz = tte_single_gompertz_fit,
 )
